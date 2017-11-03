2cf6700bac2f67f97196344228119cbe8a0491b0
Juan Jose Soriano  Escobar <Escobar@cdc.local>
2017-11-03 14:49:11 +0100

    adding git ignore and html files validation

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
 const pdf = require('html-pdf');
-const path = './htmlFiles'
+const path = './htmlFiles/'
 const options = { format: 'Letter' };
 
 fs.readdir( path, (err, items) => {
     if (items.length > 0) {
         items.forEach( i => {
-            const fileName = i.replace('.html', '');
-            const html = fs.readFileSync('./htmlfiles/' + i, 'utf8');
-            pdf.create(html, options).toFile('./pdfFiles/' + fileName + '.pdf', (err, res) => {
-                if (err) return console.log(err);
-                    console.log(res); // { filename: '/app/businesscard.pdf' }
+            if (i.toLowerCase().includes('.html')) {
+                const fileName = i.replace('.html', '');
+                const html = fs.readFileSync(path + i, 'utf8');
+                pdf.create(html, options).toFile('./pdfFiles/' + fileName + '.pdf', (err, res) => {
+                    if (err) return console.log(err);
+                    fs.unlinkSync( path + i);
                 });
+            }
         })
     }   
 });
