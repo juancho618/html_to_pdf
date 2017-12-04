00d7b49a39165efbd2bb05793aded494b741b74b
Juan Jose Soriano  Escobar <Escobar@cdc.local>
2017-12-04 15:14:23 +0100

    new BIC and IBAN clients to pay report

--- listPayment.js
+++ listPayment.js
@@ -23,30 +23,39 @@ let validPayment = toBePayed.filter(a => IBAN.isValid(a.IBAN.trim()) && bic.isVa
     Ident: item.Ident,
     IBAN: item.IBAN,
     BIC: item.BIC,
-    pay_fix: item.pay_fix
+    pay_fix: item.pay_fix,
+    email: item.Email || item.Email2
 }));
 let invalidIBAN = toBePayed.filter(a => !IBAN.isValid(a.IBAN.trim())).map( item => ({
     company: item.company_name,
     Ident: item.Ident,
     IBAN: item.IBAN,
     BIC: item.BIC,
-    pay_fix: item.pay_fix
+    pay_fix: item.pay_fix,
+    email: item.Email || item.Email2,
+    error_type: 'Invalid IBAN'
 }));
 let invalidIBIC = toBePayed.filter(a => !bic.isValid(a.BIC.trim())).map( item => ({
     company: item.company_name,
     Ident: item.Ident,
     IBAN: item.IBAN,
     BIC: item.BIC,
-    pay_fix: item.pay_fix
+    pay_fix: item.pay_fix,
+    email: item.Email || item.Email2,
+    error_type: 'Invalid BIC'
 }));
 
+const totalErrorList = invalidIBAN.concat(invalidIBIC);
 let xlPayment =   json2xls(validPayment);
-let xlInvalidIban =   json2xls(invalidIBAN);
-let xlInvalidBic =   json2xls(invalidIBIC);
+let invalidData =  json2xls(totalErrorList);
+// let xlInvalidIban =   json2xls(invalidIBAN);
+// let xlInvalidBic =   json2xls(invalidIBIC);
 
 fs.writeFileSync('to_be_paid.xlsx', xlPayment, 'binary');
-fs.writeFileSync('invalid_IBAN.xlsx', xlInvalidIban, 'binary');
-fs.writeFileSync('invalid_BIC.xlsx', xlInvalidBic, 'binary');
+fs.writeFileSync('invalid_list.xlsx', invalidData, 'binary');
+
+// fs.writeFileSync('invalid_IBAN.xlsx', xlInvalidIban, 'binary');
+// fs.writeFileSync('invalid_BIC.xlsx', xlInvalidBic, 'binary');
 
 // console.log(xlData.length);
 // console.log(toBePayed.length);
