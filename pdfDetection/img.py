3adbd74a82b3c06da761c117cb64baca4586d14a
juan <jjsorianoe@gmail.com>
2018-01-24 23:16:12 +0100

    report corrupted pdf

--- pdfDetection/img.py
+++ pdfDetection/img.py
@@ -1,44 +1,47 @@
 import numpy as np
 from PIL import Image
-
-img = Image.open('./imgs/aa_1.png')
-arr = np.array(img)
-h =arr.shape[0]
-w = arr.shape[1]
-nPixels = h*w
-dim = 0
+import sys  
 
 def count_black_1d(page):
-    nBlack = (arr == 1).sum()
-    return nBlack
+        nBlack = (arr == 1).sum()
+        return nBlack
 
-# def count_black_3d(page):
-#     h =arr.shape[0]
-#     w = arr.shape[1]
-#     dim = arr.shape[2]
-#     nBlack = 0
-#     for y in range(h):
-#         for x in range(w):
-#             if page[y][x].count(255) < 3:
-#                 nBlack += 1
-#     return nBlack
 
 def count_black_3d_transform(page):
     nBlack = (arr == False).sum()
     return nBlack
 
-if len(arr.shape) > 2:
-    dim = arr.shape[2]
-    img = img.convert('1')
+pages = sys.argv
+pages = pages[1:]
+result =[]
+white_count = 0
+black_count = 0
+for i, page in enumerate(pages):
+    img = Image.open('./imgs/'+page)
     arr = np.array(img)
-    nBlack = count_black_3d_transform(arr)
-else:
-    nBlack = count_black_1d(arr)
+    h =arr.shape[0]
+    w = arr.shape[1]
+    nPixels = h*w
+    dim = 0
+
+    
+    if len(arr.shape) > 2:
+        dim = arr.shape[2]
+        img = img.convert('1')
+        arr = np.array(img)
+        nBlack = count_black_3d_transform(arr)
+    else:
+        nBlack = count_black_1d(arr)
 
 
-black_proportion = nBlack / float(nPixels)
-if (black_proportion) > 0.7:
-    print("mostly black", black_proportion) 
+    black_proportion = nBlack / float(nPixels)
+    if (black_proportion) > 0.7:
+        black_count += 1
+    else:
+        white_count += 1
+
+black_rate = black_count/len(pages)
+print(black_rate)
 
 
 
--- pdfDetection/pdf2img.js
+++ pdfDetection/pdf2img.js
@@ -2,7 +2,9 @@
 //for the pythonshell: https://www.npmjs.com/package/python-shell
 
 let PythonShell = require('python-shell');
-const PDF2Pic = require('pdf2pic')
+const PDF2Pic = require('pdf2pic');
+const fs = require('fs.extra');
+
 const converter = new PDF2Pic({
     density: 100,           // output pixels per inch
     savename: "aa",   // output file name
@@ -10,16 +12,61 @@ const converter = new PDF2Pic({
     format: "png",          // output file format
     size: 600               // output size in pixels
 })
+// async fs function
+fs.readdirAsync = dirname => {
+    return new Promise(function(resolve, reject) {
+        fs.readdir(dirname, function(err, filenames){
+            if (err) 
+                reject(err); 
+            else 
+                resolve(filenames);
+        });
+    });
+};
 
-// by default the first page of the pdf will be converted
-// to image
-converter.convertBulk("./black.pdf", -1)
-         .then(resolve => {
-             console.log(resolve);
-             console.log(resolve.length);
-             PythonShell.run('./img.py', function (err, results) {
-                if (err) throw err;
-                console.log('results: %j', results);
-              });
-             console.log("image converted successfully");
-         })
+// get list from folder function
+let getList = (path) =>  fs.readdirAsync( path );
+let pdfList = getList('./pdfList');
+
+Promise.all([pdfList]).then( docs =>{
+    docs[0].forEach((pdf, index) =>{
+        const converter = new PDF2Pic({
+            density: 100,           // output pixels per inch
+            savename: "aa"+index,   // output file name
+            savedir: "./imgs/",    // output file location
+            format: "png",          // output file format
+            size: 600               // output size in pixels
+        })
+        converter.convertBulk(`./pdfList/${pdf}`, -1)
+        .then(resolve => {
+            let pages = resolve.map(p => p.name);
+            PythonShell.defaultOptions = { args: pages};
+            const options = {
+               mode: 'text',
+               pythonPath: 'path/to/python',
+               pythonOptions: ['-u'],
+               scriptPath: 'path/to/my/scripts',
+               args: pages
+             };
+            PythonShell.run('./img.py', function (err, results) {
+               if (err) throw err;
+               const res = results[0];
+               if (res > 0.5) {
+                   console.log('corrupted file');
+               }
+               else{
+                   console.log('good file');
+               }
+             });
+            if (index == docs[0].length - 1) { 
+                getList('./imgs').then(result =>{
+                    setTimeout(() => {
+                        result.forEach( file => fs.unlinkSync( './imgs/' + file ));
+                      }, 8000);                    
+                })
+            }             
+        }) 
+            
+    })      
+    // htmlFiles.forEach( file => fs.unlinkSync( 'H:/USER/Jobstudents/powers/' + file ));
+})
