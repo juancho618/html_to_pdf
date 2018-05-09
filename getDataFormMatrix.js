aff9d1870773ea42c5fd0b45b38a5ec3e14a63be
Juan Jose Soriano  Escobar <Escobar@cdc.local>
2018-05-09 14:00:33 +0200

    validation including preassignors

--- getDataFormMatrix.js
+++ getDataFormMatrix.js
@@ -3,14 +3,16 @@
  */
 
 const path = require('path');
+const { join } = require('path')
 const fs = require('fs.extra');
 const XLSX = require('xlsx');
 const json2xls = require('json2xls');
 const workbook = XLSX.readFile('data2.xlsm');
 const sheet_name_list = workbook.SheetNames;
 let xlData = XLSX.utils.sheet_to_json(workbook.Sheets["Tabelle1"]);
+const { lstatSync, readdirSync, statSync  } = require('fs');
 
-const workbook2 = XLSX.readFile('newList2.xlsx');
+const workbook2 = XLSX.readFile('newListNumber.xlsx');
 const sheet_name_list2 = workbook2.SheetNames;
 let xlData2 = XLSX.utils.sheet_to_json(workbook2.Sheets["Sheet 1"]);
 
@@ -32,16 +34,46 @@ fs.readdirAsync = dirname => {
 let getList = (path) =>  fs.readdirAsync( path );
 
 
-const CPA = getList('H:\\USER\\Jobstudents\\CPAs');
-// const DoT = getList('H:\\USER\\Jobstudents\\DoTs\\DoT_2sig_Stamped'); //Not included for now
-const DoTo = getList('H:\\USER\\Jobstudents\\DoTs\\Dot_o_Stamped');
-const powers = getList('H:\\USER\\Jobstudents\\powersList');
+  //Extracts Id number exluding the underscore and the first two letters!
+  let tempId = (id) =>{
+    const reLetters = new RegExp('^[a-zA-Z]{2}');
+
+    if (reLetters.exec(id)){ // if it starts with two letters
+        if (id.includes('_')){
+            return id.slice(3);
+        } else {
+            return id.slice(2);
+        }
+    }
+    return id;
+};
+
+
+
+//To get the list of the directories inside of a directory
+const isDirectory = source => lstatSync(source).isDirectory()
+const getDirectories = source =>  readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+//new dir method
+const dirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isDirectory())
+
+const CPA = getList('H:\\USER\\Jobstudents\\Trucks\\CPAs');
+const DoT = getList('H:\\USER\\Jobstudents\\Trucks\\DoTs\\DoT_2sig_Stamped'); //Not included for now
+const DoTo = getList('H:\\USER\\Jobstudents\\Trucks\\DoTs\\Dot_o_Stamped');
+const powers = getList('H:\\USER\\Jobstudents\\Trucks\\powersList');
+const preAssigments = getList('H:\\USER\\Jobstudents\\Trucks\\pre-assignments');
 // const HtmltoPDF = getList('H:\\USER\\Jobstudents\\powers\\HtmltoPDF');
 // const ocrError = getList('H:\\USER\\Jobstudents\\powers\\OCR-PDF-A\\error');
 // const ocrGodd =  getList('H:\\USER\\Jobstudents\\powers\\OCR-PDF-A\\good');
 // const ocrToBe =  getList('H:\\USER\\Jobstudents\\powers\\OCR-PDF-A\\to be converted');
 
-Promise.all([CPA, powers, DoTo]).then( values =>{
+let suspiciousFiles = [];
+let missing = [];
+let assignors = [];
+let preassignors = []; 
+let isPreassignor = false;
+
+
+Promise.all([CPA, powers, DoTo, preAssigments, DoT]).then( values =>{
     // creating the folders
     const destination = 'C:\\Users\\Escobar\\Desktop\\newList2'; //'H:\\USER\\Jobstudents\\DocumentsList'
     
@@ -49,108 +81,184 @@ Promise.all([CPA, powers, DoTo]).then( values =>{
 
     let numbered = xlData2.filter(x => x.hasOwnProperty('company_name'));
     console.log(numbered.length);
-    let suspiciousFiles = [];
-    let missing = [];
-
+    
     
     numbered.forEach(i => {
         let documentsFound = [];
         i.company_name = i.company_name.replace('/', '.'); //to avoid subfoldering
-
-        //Folder structure name
-        const savePath = `${destination}/${i.company_name} - ${i.Ident}`; //${i['Assigner_Number_writ ']} -
+        i.company_name = i.company_name.replace('\\r', '');
         
-        //Extracts Id number
-        let tempId = (id) =>{
-            const reLetters = new RegExp('^[a-zA-Z]{2}');
 
-            if (reLetters.exec(id)){ // if it starts with two letters
-                if (id.includes('_')){
-                    return id.slice(3);
-                } else {
-                    return id.slice(2);
+        //Get PreAssignors
+        if (i.Assig_Number_writ && (i.Assig_Number_writ != i.Assignor_Number)){
+            preassignors.push(i);
+            isPreassignor = true;
+        } else {
+            assignors.push(i);
+            isPreassignor = false;
+        }
+
+        //The idea is to save first the list of the Assignors
+        if (isPreassignor == false){
+            //Folder structure name
+            const savePath = `${destination}/${i.Assignor_Number} - ${i.company_name} - ${i.Ident}`; //${i['Assigner_Number_writ ']} -
+            const extractId = tempId(i.Ident.trim());
+
+            let missDoc = {target: i.company_name, 
+                originalId: i.Ident, usedId: extractId, 
+                CPA: true, DOT: true, Power: true, Preassigment: 'N/A', 
+                idType: i.Ident_type,cpaMatch: false,dotMatch: false,powerMatch: false, preassigmentMatch: false, 
+                Assig_Number_writ: i['Assig_Number_writ'], 
+                Assignor_Number: i['Assignor_Number'],
+                isPreassignor: false}
+
+            if (!fs.existsSync(savePath)){
+                fs.mkdirSync(savePath);
+            }
+            // CPA
+            values[0].forEach(d => {
+                if (d.includes(extractId)) {
+                    fs.copy('H:\\USER\\Jobstudents\\Trucks\\CPAs' + '/' + d, `${savePath}/${d}`, { replace: false });
+                    documentsFound.push('CPA');
                 }
+                else if (d.includes(i.company_name)){
+                    missDoc.cpaMatch = true;
+                    suspiciousFiles.push({file: d,type:'CPA', target: i.company_name, originalId: i.Ident, usedId: extractId});
+                }
+            })
+
+            // Powers
+            values[1].forEach(d => {
+                if (d.includes(extractId)) {
+                    fs.copy('H:\\USER\\Jobstudents\\Trucks\\powersList' + '/' + d, `${savePath}/${d}`, { replace: false });
+                    documentsFound.push('Power');
+                }
+                else if (d.includes(i.company_name)){ //Could not be find by ID but it is present with the company name
+                    missDoc.powerMatch = true;
+                    suspiciousFiles.push({file: d,type:'Powers',  target: i.company_name, originalId: i.Ident, usedId: extractId});
+                }
+            })
+
+            // DoT_o_Stamped
+            values[2].forEach(d => {
+                if (d.includes(extractId)) {
+                    fs.copy('H:\\USER\\Jobstudents\\Trucks\\DoTs\\Dot_o_Stamped' + '/' + d, `${savePath}/${d}`, { replace: false });
+                    documentsFound.push('DOT');
+                }
+                else if (d.includes(i.company_name)){
+                    missDoc.dotMatch = true;
+                    suspiciousFiles.push({file: d,type:'DOT',  target: i.company_name, originalId: i.Ident, usedId: extractId});
+                }
+            })
+
+            // DoT_2sig_stamped
+            values[4].forEach(d => {
+                if (d.includes(extractId)) {
+                    fs.copy('H:\\USER\\Jobstudents\\Trucks\\DoTs\\DoT_2sig_Stamped' + '/' + d, `${savePath}/${d}`, { replace: false });
+                    documentsFound.push('DOT');
+                }
+                else if (d.includes(i.company_name)){
+                    missDoc.dotMatch = true;
+                    suspiciousFiles.push({file: d,type:'DOT',  target: i.company_name, originalId: i.Ident, usedId: extractId});
+                }
+            })
+
+            if (!documentsFound.includes('DOT') || !documentsFound.includes('CPA') || !documentsFound.includes('Power')){
+                
+                if (!documentsFound.includes('DOT'))
+                    missDoc.DOT = false;
+                if (!documentsFound.includes('CPA'))
+                    missDoc.CPA = false;
+                if (!documentsFound.includes('Power'))
+                    missDoc.Power = false;
+
+                missing.push(missDoc);
             }
-            return id;
 
-        };
-
-        const extractId = tempId(i.Ident.trim());
-        // console.log(`${i.Ident}, ${extractId}`);
-        let missDoc = {target: i.company_name, originalId: i.Ident, usedId: extractId, CPA: true, DOT: true, Power: true, idType: i.Ident_type,cpaMatch: false,dotMatch: false,powerMatch: false, Assig_Number_writ: i['Assig_Number_writ']}
-
-        if (!fs.existsSync(savePath)){
-            fs.mkdirSync(savePath);
-            // fs.mkdirSync(`${savePath}/CPA`);
-            // fs.mkdirSync(`${savePath}/DOT`);
-            // fs.mkdirSync(`${savePath}/powers`);
-        }
-
-        // CPA
-        values[0].forEach(d => {
-            if (d.includes(extractId)) {
-                fs.copy('H:\\USER\\Jobstudents\\CPAs' + '/' + d, `${savePath}/${d}`, { replace: false });
-                documentsFound.push('CPA');
-            }
-            else if (d.includes(i.company_name)){
-                missDoc.cpaMatch = true;
-                suspiciousFiles.push({file: d,type:'CPA', target: i.company_name, originalId: i.Ident, usedId: extractId});
-            }
-        })
-
-        // // DOT
-        // values[1].forEach(d => {
-        //     if (d.includes(extractId)) {
-        //         fs.copy('H:\\USER\\Jobstudents\\DoTs\\DoT_2sig_Stamped' + '/' + d, `${savePath}/${d}`, { replace: false });
-        //     }
-        // })
-
-         // Powers
-         values[1].forEach(d => {
-            if (d.includes(extractId)) {
-                fs.copy('H:\\USER\\Jobstudents\\powersList' + '/' + d, `${savePath}/${d}`, { replace: false });
-                documentsFound.push('Power');
-            }
-            else if (d.includes(i.company_name)){
-                missDoc.powerMatch = true;
-                suspiciousFiles.push({file: d,type:'Powers',  target: i.company_name, originalId: i.Ident, usedId: extractId});
-            }
-        })
-
-        // DoT_o_Stamped
-        values[2].forEach(d => {
-            if (d.includes(extractId)) {
-                fs.copy('H:\\USER\\Jobstudents\\DoTs\\Dot_o_Stamped' + '/' + d, `${savePath}/${d}`, { replace: false });
-                documentsFound.push('DOT');
-            }
-            else if (d.includes(i.company_name)){
-                missDoc.dotMatch = true;
-                suspiciousFiles.push({file: d,type:'DOT',  target: i.company_name, originalId: i.Ident, usedId: extractId});
-            }
-        })
-
-        if (!documentsFound.includes('DOT') || !documentsFound.includes('CPA') || !documentsFound.includes('Power')){
-            
-            if (!documentsFound.includes('DOT'))
-                missDoc.DOT = false;
-            if (!documentsFound.includes('CPA'))
-                 missDoc.CPA = false;
-            if (!documentsFound.includes('Power'))
-                missDoc.Power = false;
-
-            missing.push(missDoc);
-        }
+        }    
     })
+    
+    //Once all teh assignors are processed the Idea is to process the pre-assignors
+    preassignors.forEach(pre => {
+        let documentsFound = [];
+        console.log('pre', pre.company_name);
+        const extractId = tempId(pre.Ident.trim());
+        const AssignorId = tempId(pre.pre_assig_of.trim()); //Main assignor Id
+        const directoriesList = dirs(`${destination}`);
+        const parentDir = directoriesList.filter(dir => dir.includes(AssignorId));
+        console.log(parentDir);
 
-    /* create sheet data & add to workbook for the json file */
-    let data =   json2xls(suspiciousFiles);
-    fs.writeFileSync('secondList.xlsx', data, 'binary');
+        let missDoc = {
+            target: pre.company_name, originalId: pre.Ident, 
+            usedId: extractId, CPA: true, 
+            DOT: true, Power: true, 
+            Preassigment: 'N/A', predType: pre.Ident_type,cpaMatch: false,dotMatch: false,powerMatch: false, preassigmentMatch: false,
+            Assig_Number_writ: pre['Assig_Number_writ'],
+            Assignor_Number: pre['Assignor_Number'],
+            isPreassignor: true};
 
-    /* create sheet data & add to workbook for the json file */
-    let datam =   json2xls(missing);
-    fs.writeFileSync('missigList.xlsx', datam, 'binary');
+        Promise.all([powers, preAssigments]).then(v=>{
+
+            // Preassignor power
+            v[0].forEach(d => {
+                if (d.includes(extractId)) {
+                    fs.copy('H:\\USER\\Jobstudents\\Trucks\\powersList' + '/' + d, `${destination}/${parentDir}/${d}`, { replace: false });
+                    documentsFound.push('Power');
+                }
+                else if (d.includes(pre.company_name)){
+                    missDoc.powerMatch= true;
+                    suspiciousFiles.push({file: d,type:'Powers-preAssignor',  target: pre.company_name, originalId: pre.Ident, usedId: extractId});
+                }
+            })
+            
+            //For the preassigments
+            v[1].forEach(d => {
+                if (d.includes(extractId)) {
+                    fs.copy('H:\\USER\\Jobstudents\\Trucks\\pre-assignments' + '/' + d, `${destination}/${parentDir}/${d}`, { replace: false });
+                    documentsFound.push('Preassigment');
+                }
+                else if (d.includes(pre.company_name)){
+                    missDoc.preassigmentMatch = true;
+                    suspiciousFiles.push({file: d,type:'preassigment', target: pre.company_name, originalId: pre.Ident, usedId: extractId});
+                }
+            })
+
+            //Add validation for missing documents for the pre-Assignor
+            if ( !documentsFound.includes('Preassigment') || !documentsFound.includes('Power')){
+                
+                if (!documentsFound.includes('Preassigment'))
+                    missDoc.Preassigment = false;
+                if (!documentsFound.includes('Power'))
+                    missDoc.Power = false;
+                missing.push(missDoc);
+            }
+
+            //TO BE REFACTORED!!!
+            console.log('Total Number Pre-Assignors', preassignors.length );
+            console.log('Total Number Assignors', assignors.length);
+
+            /*creates an excel file with the assignors and another one with the assignors*/
+            const assignorsList = json2xls(assignors);
+            const preassignorsList = json2xls(preassignors);
+            fs.writeFileSync('assignorsList.xlsx', assignorsList, 'binary');
+            fs.writeFileSync('preassignorsList.xlsx', preassignorsList, 'binary');
+
+            /* create sheet data & add to workbook for the json file */
+            let data =   json2xls(suspiciousFiles);
+            fs.writeFileSync('secondList.xlsx', data, 'binary');
+
+            /* create sheet data & add to workbook for the json file */
+            let datam =   json2xls(missing);
+            fs.writeFileSync('missigList.xlsx', datam, 'binary');
+        })
+        
+
+    });
+        
+   
     
     
 
-})
+
+}).catch(e => console.error('error', e));
 
--- newAssirgnorList.js
+++ newAssirgnorList.js
@@ -2,14 +2,15 @@ const XLSX = require('xlsx');
 const json2xls = require('json2xls');
 const fs = require('fs.extra');
 
-
 const workbook = XLSX.readFile('data2.xlsm');
 const sheet_name_list = workbook.SheetNames;
 let xlData = XLSX.utils.sheet_to_json(workbook.Sheets["Tabelle1"]);
 
 let newAssignors = xlData.filter(assignor => {
-    
-    if ( assignor.hasOwnProperty('company_name') && assignor.hasOwnProperty('signdate_CDC_CPA ')  && assignor.hasOwnProperty('signdate_assig_CPA') ) { //other val: && !assignor.signdate_CDC_DoT.includes('still open') assignor.hasOwnProperty('signdate_CDC_DoT') && assignor.hasOwnProperty('signdate_assig_DoT')
+    // && assignor.hasOwnProperty('Assignor_Number')
+    if ( assignor.hasOwnProperty('company_name') && assignor.hasOwnProperty('Assignor_Number')  ) { //other val: && !assignor.signdate_CDC_DoT.includes('still open') assignor.hasOwnProperty('signdate_CDC_DoT') && assignor.hasOwnProperty('signdate_assig_DoT') && assignor.hasOwnProperty('signdate_CDC_CPA ')  && assignor.hasOwnProperty('signdate_assig_CPA')
+   
+   
     //   if ( assignor.hasOwnProperty('Assig_included_in_writ') ){
     //         if (!assignor.Assig_included_in_writ.toUpperCase().includes('YES')){
     //             return assignor;
@@ -18,28 +19,33 @@ let newAssignors = xlData.filter(assignor => {
     //         return assignor; //some reduncancy??
     //     } && assignor.hasOwnProperty('signdate_CDC_CPA') && assignor.hasOwnProperty('signdate_assig_CPA')
     
-        const cpaCdcDate = (assignor['signdate_CDC_CPA '].includes('17') || assignor['signdate_CDC_CPA '].includes('18')); 
-        const cpaAssignorDate = (assignor['signdate_assig_CPA'].includes('17') || assignor['signdate_assig_CPA'].includes('18'));
-        if ( cpaCdcDate &&  cpaAssignorDate){
-            if ( assignor.hasOwnProperty('Included_by_writ_of')){
-                console.log(assignor['Included_by_writ_of']);
-                if (assignor.Included_by_writ_of.toUpperCase().includes('DEC')){
-                                return assignor;
-                }
-            } else { //or empty
-                return assignor;
-            }
-        }       
-
-    }
+    //     const cpaCdcDate = (assignor['signdate_CDC_CPA '].includes('17') || assignor['signdate_CDC_CPA '].includes('18')); 
+    //     const cpaAssignorDate = (assignor['signdate_assig_CPA'].includes('17') || assignor['signdate_assig_CPA'].includes('18'));
+       // if ( cpaCdcDate &&  cpaAssignorDate){
+            // if ( assignor.hasOwnProperty('Included_by_writ_of')){
+                
+            //     if (assignor['Included_by_writ_of'] != 'not claiming')
+            //         console.log(assignor['pre_assig_of']);
+            //         return assignor;
+            // }
+                // if (assignor.Included_by_writ_of.toUpperCase().includes('DEC')){
+                //                 return assignor;
+                // }
+            // } else { //or empty
+            //     return assignor;
+            // }
+        //}       
+        if (assignor.Assignor_Number > 0 || assignor.Assignor_Number == '331a'){
+            if (assignor.pre_assig_of)
+                 assignor.pre_assig_of;
+            else
+                assignor.pre_assig_of = '';
+            return assignor;
+        }
+            
+     }
 });
 
 let newList =   json2xls(newAssignors);
- console.log(newAssignors.length);
- fs.writeFileSync('newList2.xlsx', newList, 'binary');
-
-// let xlInvalidIban =   json2xls(invalidIBAN);
-// let xlInvalidBic =   json2xls(invalidIBIC);
-
-// fs.writeFileSync('to_be_paid.xlsx', xlPayment, 'binary');
-// fs.writeFileSync('invalid_list.xlsx', invalidData, 'binary');
+console.log(newAssignors.length);
+fs.writeFileSync('newListNumber.xlsx', newList, 'binary');
